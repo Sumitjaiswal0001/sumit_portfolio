3837419f01ae93f44d5c4eabce7b21e07e2db557
sumit kumar jaiswal <145224352+Sumitjaiswal0001@users.noreply.github.com>
2024-12-10 22:13:16 +0530

    works.js

--- src/components/works/works.js
+++ src/components/works/works.js
@@ -16,8 +16,7 @@ const Works = () => {
       <a href="https://shoes-store-roan.vercel.app/#nav" target='blank'><img src={Portfolio1} alt="" className="workImg" /> </a>
       <a href="https://cloth-store-zeta.vercel.app/" target='blank'><img src={Portfolio2} alt="" className="workImg" /></a>
       <a href="https://blog-three-peach-61.vercel.app/" target="_blank"><img src={Portfolio4} alt="" className="workImg" /></a>
-        
-        <img src={Portfolio3} alt="" className="workImg" />
+       <a href="https://calculater-eight-lime.vercel.app/" target="_blank"><img src={Portfolio3} alt="" className="workImg" /></a>
         <img src={Portfolio5} alt="" className="workImg" />
         <img src={Portfolio6} alt="" className="workImg" />
       </div>
